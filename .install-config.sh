43babe36f768c0bb497816787cfb04ba1a2f0198
LPAube <lpaube@hotmail.com>
2021-12-25 00:05:53 -0500

    mods

--- .install-config.sh
+++ .install-config.sh
@@ -17,7 +17,9 @@ config config --local status.showUntrackedFiles no
 
 #Creating backup dotfiles directory
 mkdir -p $HOME/.config-backup
-mkdir -p $HOME/.config-backup/i3
+mkdir -p $HOME/.config-backup/.config
+mkdir -p $HOME/.config-backup/.config/i3
+
 config checkout 2>&1 | egrep "\s+" | sed -n '1!p' | sed -n '$!p' | awk {'print $1'} | \
 	xargs -I% mv $HOME/% $HOME/.config-backup/%
 
