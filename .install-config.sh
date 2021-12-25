654dedfdad045bfb84c922d9b91480aa83a6a2e0
LPAube <lpaube@hotmail.com>
2021-12-24 23:50:49 -0500

    fixed install script

--- .install-config.sh
+++ .install-config.sh
@@ -6,16 +6,20 @@ function config {
 	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
+#Removing pre-existing $HOME/.cfg
+rm -rf $HOME/.cfg
+
+#Cloning
 git clone --bare --branch linux git@github.com:lpaube/.dotfiles.git $HOME/.cfg
 
 #Config git to not show unnecessary files
 config config --local status.showUntrackedFiles no
 
 #Creating backup dotfiles directory
-mkdir -p $HOME/.config-backup && config checkout 2>&1 \
-| egrep "\s+" | sed -n '1!p' | sed -n '$!p' | awk {'print $1'} | \
-xargs -I{} mv {} $HOME/.config-backup/{}
+mkdir -p $HOME/.config-backup
+mkdir -p $HOME/.config-backup/i3
+config checkout 2>&1 | egrep "\s+" | sed -n '1!p' | sed -n '$!p' | awk {'print $1'} | \
+	xargs -I% mv $HOME/% $HOME/.config-backup/%
 
 #Get the dotfiles and self-destruct
 config checkout
-#rm -rf $HOME/README.md
